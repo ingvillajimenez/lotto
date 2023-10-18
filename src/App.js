edec792e3129b7d80a44c875dd986ef03c951fff
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-18 10:44:24 -0600

    State Design Example: Lottery Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Ball from "./Ball";
+import Lottery from "./Lottery";
 
 function App() {
   return (
     <div className="App">
-      <Ball num={17} />
-      <Ball num={13} />
-      <Ball num={11} />
-      <Ball num={9} />
+      <Lottery />
+      <Lottery title="Mini Daily" maxNum={10} numBalls={4} />
     </div>
   );
 }
